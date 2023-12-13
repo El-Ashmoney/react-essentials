a7f6f65a904a204180981568037ceebfc81cc41c
Mohammed Ragab El Ashmoney <98624415+El-Ashmoney@users.noreply.github.com>
2023-12-13 10:48:06 +0200

    Creating the components folder and adding the example buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
+import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/TabButton/TabButton.jsx';
 
 
 function App() {
+    function handleSelect() {
+        console.log('Selected');
+    }
     return (
         <div className="App">
             <main>
@@ -19,15 +22,16 @@ function App() {
                         <CoreConcept {...CORE_CONCEPTS[2]} />
                         <CoreConcept {...CORE_CONCEPTS[3]} />
                     </ul>
-                <section id="examples">
-                    <h2>Examples</h2>
-                    <menu>
-                        <TabButton>Components</TabButton>
-                        <TabButton>JSX</TabButton>
-                        <TabButton>Props</TabButton>
-                        <TabButton>State</TabButton>
-                    </menu>
-                </section>
+                    <section id="examples">
+                        <h2>Examples</h2>
+                        <menu>
+                            <TabButton onSelect={handleSelect}>Components</TabButton>
+                            <TabButton onSelect={handleSelect}>JSX</TabButton>
+                            <TabButton onSelect={handleSelect}>Props</TabButton>
+                            <TabButton onSelect={handleSelect}>State</TabButton>
+                        </menu>
+
+                    </section>
                 </section>
             </main>
         </div>
