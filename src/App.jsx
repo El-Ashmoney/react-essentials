8847d3d8fa4757153ff1c7ae736cc794de8603de
Mohammed Ragab El Ashmoney <98624415+El-Ashmoney@users.noreply.github.com>
2023-12-18 16:38:46 +0200

    adding the logic for the app.jsx and adding the dynamic content in data.js modifying the css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import { useState } from 'react';
 import './App.css';
-import { CORE_CONCEPTS } from './data.js';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/TabButton/TabButton.jsx';
 
 
 function App() {
-    const [ selectedTopic, setSelectedTopic ] = useState();
+    const [ selectedTopic, setSelectedTopic ] = useState('components');
     function handleSelect(selectedButtons) {
         setSelectedTopic(selectedButtons);
     }
@@ -32,7 +32,15 @@ function App() {
                             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
                             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
                         </menu>
-                        {selectedTopic}
+                        <div id="tab-content">
+                            <h3>{EXAMPLES[selectedTopic].title}</h3>
+                            <div id="content">
+                                <p>{EXAMPLES[selectedTopic].description}</p>
+                                <pre>
+                                    <code>{EXAMPLES[selectedTopic].code}</code>
+                                </pre>
+                            </div>
+                        </div>
                     </section>
                 </section>
             </main>
